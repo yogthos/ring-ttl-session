8c022e039422958857dc99af3fc9e78c6dc64ad6
Yogthos <dmitri.sotnikov@gmail.com>
2015-09-13 11:41:56 -0400

    updated to use expiring-map

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :url "https://github.com/boechat107/ring-ttl-session"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring/ring-core "1.4.0"]
-                 [org.clojure/core.cache "0.6.4"]]
+                 [expiring-map "0.1.6"]]
   :profiles
   {:dev
    {:global-vars {*warn-on-reflection* true}}})
--- src/ring_ttl_session/core.clj
+++ src/ring_ttl_session/core.clj
@@ -1,23 +1,23 @@
 (ns ring-ttl-session.core
-  (:require [clojure.core.cache :refer [ttl-cache-factory]]
+  (:require [expiring-map.core :as em]
             [ring.middleware.session.store :refer [SessionStore]]))
 
-(deftype TTLMemoryStore [cache-atom]
+(deftype TTLMemoryStore [cache]
   SessionStore
   (read-session [_ k]
-    (when-let [session (get @cache-atom k)]
-      (swap! cache-atom assoc k session)
+    (when-let [session (get cache k)]
+      (em/assoc! cache k session)
       session))
   (write-session [_ k data]
     (let [k (or k (str (java.util.UUID/randomUUID)))]
-      (swap! cache-atom assoc k data)
+      (em/assoc! cache k data)
       k))
   (delete-session [_ k]
-    (swap! cache-atom dissoc k)
+    (em/dissoc! cache k)
     nil))
 
 (defn ttl-memory-store
   "Returns an implementation of SessionStore where sessions have a time-to-live given
   in seconds."
-  [ttl]
-  (TTLMemoryStore. (atom (ttl-cache-factory {} :ttl (* 1000 ttl)))))
+  [ttl & [opts]]
+  (TTLMemoryStore. (em/expiring-map ttl (merge {:expiration-policy :access} opts))))
